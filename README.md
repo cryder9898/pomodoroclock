7bc1231ab6783948696be660692df866d3cb9986
cwryd <cryder9898@gmail.com>
2017-02-13 16:41:12 -0500

    added buttons and formatting and have a timer display with almost functional countdown

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pomodoroclock
+
+This is a FreeCodeCamp Zipline to implement a Pomodoro Clock using HTML, CSS, and javascript.
+
+I am using the Bootstrap framework to help with formatting and styling.
--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 $(document).ready(function () {
-	var sec = 1000;
-	var i = 1
-	var count = setInterval(function () {
-		$('#clock').text(i++);
-	}, sec);
+	var delay = 1000;
+	var totalSeconds = 25 * 60;
+	var breakMin = 5;
+	var sessMin = 25;
+	var isRunning = false;
+	var timer;
+
+	var setTime = function () {	
+    	$('#secs').text(totalSeconds % 60);
+    	$('#mins').text(parseInt(totalSeconds / 60));
+    	totalSeconds--;
+	};
+
+	var startSess = function () {
+		if (!timer) {
+      		timer = setInterval(setTime, delay);
+    	}
+	};
+
+	$('.timer').click(function () {
+		if(isRunning) {
+			isRunning = false;
+			clearInterval(timer);
+		} else {
+			isRunning = true;
+			startSess();
+		}	
+	})
+
 });
+	
