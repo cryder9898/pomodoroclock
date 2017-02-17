967ef8ed62d15ffc5ca92bcae0ccc548e5fa0af1
cwryd <cryder9898@gmail.com>
2017-02-17 11:02:27 -0500

    added correct number of digits for timer and also added functionality for both session and break by passing parameter to setInterval methfunction myFunction() {

--- js/script.js
+++ js/script.js
@@ -1,32 +1,83 @@
-$(document).ready(function () {
-	var delay = 1000;
-	var totalSeconds = 25 * 60;
+
+var delay = 1000;
+	var sessMin = 1;
 	var breakMin = 5;
-	var sessMin = 25;
+	var sessSecs = sessMin * 60;
 	var isRunning = false;
 	var timer;
+$(document).ready(function () {
+		
+	function displayTime(time) {
+		let mins = parseInt(time / 60);
+		if (mins < 10) {
+			mins = '0' + mins;
+		}
+		$('#mins').text(mins);
+		let secs = time % 60;	
+		if (secs < 10) {
+			secs = '0' + secs;
+		}
+    	$('#secs').text(secs);
+	}
 
-	var setTime = function () {	
-    	$('#secs').text(totalSeconds % 60);
-    	$('#mins').text(parseInt(totalSeconds / 60));
-    	totalSeconds--;
-	};
+	function countDown(time) {
+		return function () { displayTime(--time); };	
+	}
 
-	var startSess = function () {
-		if (!timer) {
-      		timer = setInterval(setTime, delay);
-    	}
-	};
+	function runTimer(time) {
+      	timer = setInterval(countDown(time), delay);
+	}
 
+	// init timer values
+	displayTime(sessSecs);
+	$('#break-val').text(breakMin);
+	$('#session-val').text(sessMin);
+
+	// increment break by 1
+	$('#add-br').click(function () {
+		$('#break-val').text(++breakMin);
+	});
+
+	// decrease break by 1
+	$('#sub-br').click(function () {
+		$('#break-val').text(--breakMin);
+	});
+
+	// increase session by 1
+	$('#add-sess').click(function () {
+		$('#session-val').text(++sessMin);
+		clearInterval(timer);
+		sessSecs = sessMin * 60;
+		displayTime(sessSecs);
+	});
+
+	// decrease session by 1
+	$('#sub-sess').click(function () {
+		$('#session-val').text(--sessMin);
+		clearInterval(timer);
+		sessSecs = sessMin * 60;
+		displayTime(sessSecs);
+	});
+
+	// start and pause timer
 	$('.timer').click(function () {
 		if(isRunning) {
 			isRunning = false;
 			clearInterval(timer);
 		} else {
 			isRunning = true;
-			startSess();
+			runTimer(sessSecs);
 		}	
-	})
+	});
 
 });
-	
+
+function orderMyLogic(val) {
+if (val < 5 ) {
+return "Less than 5";
+}else if(val < 10){
+return "less than 10";
+}else {
+return "Greater than or equal to 10";
+}
+}
